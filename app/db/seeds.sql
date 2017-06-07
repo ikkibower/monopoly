9ce72bece06d933cd8fbb70f500c78d073b067a3
Elliott Chalmers <elliottchalmers@wireless-v47-press00239.dhcp.unc.edu>
2017-06-06 21:27:46 -0400

    part one of board

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -1,3 +1,13 @@
 SELECT * FROM properties;
+-- Row 1 
+INSERT INTO properties VALUES (0,"Go","Go",null,null,null,null,null);
+INSERT INTO properties VALUES (1,'Mediteranean Avenue','Property',false,null,60,30,2);
+INSERT INTO properties VALUES (2,"Community Chest","Chest",null,null,null,null,null);
+INSERT INTO properties VALUES (3,'Baltic Avenue','Property',false,null,60,30,4);
+INSERT INTO properties VALUES (4,"Income Tax","Tax",null,null,null,null,null);
+INSERT INTO properties VALUES (5,"Reading Railroad","RR",false,null,null,null,null);
+INSERT INTO properties VALUES (6,"Oriental Ave","Property",false,null,100,50,6);
+INSERT INTO properties VALUES (7,"Chance","Chance",null,null,null,null,null);
+INSERT INTO properties VALUES (8,"Vermont Ave","Property",false,null,100,50,6);
+INSERT INTO properties VALUES (9,"Connecticut Ave","Property",false,null,120,60,8);
 
-INSERT INTO properties VALUES (0,"Go","Go",null,null,60,2,30);
