dcad460efc542b79d7e0ecdc2a32ed244bf1e902
Elliott Chalmers <elliottchalmers@guest-2-00439.dhcp.unc.edu>
2017-06-15 20:08:26 -0400

    stuff

--- app/public/assets/js/function.js
+++ app/public/assets/js/function.js
@@ -6,55 +6,47 @@ var boardValues = [];
 var snapPieces = [];
 var activePlayer = "";
 
+
 // Snap.svg Setup
-var piece;
-var s = Snap('#svg');
+var s = Snap('#svg'),
+    boardSet = Snap.set(s),
+    pieceSet = Snap.set();
 var die = Snap('#dice-svg');
 // s.attr({ viewBox: "0 0 600 900" });
-var hat = Snap.select('#hat');
-
-// hat.stop().animate({ transform: 't0,50'}, 200, mina.easeout);
-// var board = s.board();
-var loop = "M39.148,1387.667l7.018-210.542l0-1130.483h1326v1137.5l8.84,201.047l-0.544-0.021l-187.815-7.525h-942.98L39.148,1387.667z M47.166,47.642v1129.5l-6.982,209.475l209.458-9.974l943.024-0.001l187.293,7.504l-8.792-199.981l-0.001-1136.522H47.166z";
-var loopLength = Snap.path.getTotalLength(loop);
 
 // var board = s.image("monopoly.svg", 0, 0, 600, 600);
 var board = Snap.load("", function(loadedFragment) {
     s.image("assets/img/monopoly.svg", 0, 0, 600, 600);
     s.image("assets/img/path.svg", 0, 0, 600, 600);
     s.append(loadedFragment);
+    var loop = s.path("M 20 18 L 575 18 L 575 580 L 20 580  Z").attr({ fill: "none", stroke: "red", opacity: "1" });
+
+    var rect = s.rect(60, 0, 20, 20).attr({ fill: 'blue', opacity: 0 });
+    var rect2 = rect.clone();
+
+    // hat.transform('t625,-150');
+    //     var t = new Snap.Matrix()
+    // t.translate(100, 100);
+    // hat.transform(t); 
+    // function drawRect(el) {
+    //     el.drawAtPath(loop, 7000, { callback: drawRect.bind(null, el) });
+    // }
+
+    // for (var x = 0; x < 1; x++) {
+    //     setTimeout(function() { drawRect(hat.clone().attr({ opacity: 1 })) }, x * 1000);
+    // }
+
+    // var loopLength = Snap.path.getTotalLength(loop);
 });
+
+
+
+
 // TESTING PIECE ON BOARD
 $('#submit').on('click', function() {
     event.preventDefault();
-    $('#hat').show();
-    hat.marker(540, 540, 600, 600);
-    piece = Snap.load("", function(loadedFragment) {
-        s.image("assets/img/car.svg", 540, 540, 50, 50);
-        s.append(loadedFragment);
-    });
-});
-// piece.click(function(e) {
-//     Snap.animate(0, loopLength,
-//         function(step) { //step function
-//             console.log('step', step);
 
-//             circleOutline.attr({
-//                 path: Snap.path.getSubpath(loop, 0, step)
-//             });
-//         }, // end of step function
-//         800, //duration
-//         mina.easeInOut, //easing
-//         function() { //callback
-//             setTimeout(function() {
-//                 circleOutline.attr({
-//                     path: Snap.path.getSubpath(loop, 0, 0),
-//                     strokeWidth: 0
-//                 });
-//             }, 1000); //setTimeout
-//         } //callback
-//     ); //Snap.animate
-// }); //click the circle
+});
 
 
 
@@ -65,16 +57,37 @@ $('#submit').on('click', function() {
 $('#roll').on("click", function() {
     event.preventDefault();
     rollDice();
+
 });
 $('#start').on('click', function(req, res) {
     event.preventDefault();
     getPlayer();
     getBoard();
+    piece = Snap.load("", function(loadedFragment) {
+        // Creating group <g>
+        g = s.g();
+        for (var i = 0; i < globalPlayers.length; i++) {
+            var img = s.image(`assets/img/${globalPlayers[i].piece}.svg`, 545, 545, 50, 50);
+            img.attr({
+                id: `snap-${globalPlayers[i].piece}`
+            });
+            snapPieces.push(img);
+            g.add(img);
+            s.select(`snap-${globalPlayers[i].piece}`);
+            snapPieces[i].transform('t0,-300');
+        }
+
+        s.append(loadedFragment);
+        console.log(s);
+        console.log(snapPieces);
+        promptPlayer();
+    });
 });
 /*====     Game Functions     =====
  */
 // Roll Dice Function
 function rollDice(spaces) {
+    $('#roll').hide();
     var dice = [];
     spaces = 0;
     for (i = 0; i < 2; i++) {
@@ -102,24 +115,22 @@ function playerSort() {
 // Choose Playing Order
 function playingOrder() {
     $('#start').hide();
-    $('#roll').removeClass("hidden");
-    event.preventDefault();
+    // event.preventDefault();
     playerSort();
     globalPlayers[0].active_turn = true;
     activePlayer = globalPlayers[0];
-    // playerTurn();
 
 }
 // Move Piece
 function movePiece(id) {
-    hat.animate({ transform: 't0,50' }, 200, mina.easeout, function() {
+    hat.animate({ transform: 't100,50' }, 2000, mina.easeout, function() {
 
     });
 }
 
 // Update globalPlayers object on move
 function updateGlobal(spaces) {
-    for (i = 0; i < globalPlayers.length + 1; i++) {
+    for (i = 0; i < globalPlayers.length+1; i++) {
         var next = i + 1;
         if (globalPlayers[i].active_turn === true && next < globalPlayers.length) {
             console.log("Your move: " + globalPlayers[i].player_name);
@@ -165,11 +176,26 @@ function updateGlobal(spaces) {
         console.log(globalPlayers);
     }
 }
+/* PROMPT PLAYER
+ */
+function promptPlayer() {
+    $('#buy-opt').hide();
+    $('#roll').show();
+    for (i = 0; i < globalPlayers.length; i++) {
+        if (globalPlayers[i].active_turn === true) {
+            $('#prop-info').html(`
+                <h3>Your Move ${globalPlayers[i].player_name}                
+                `);
 
+        }
+    }
+}
 
+/* GET BOARD VALUES
+ */
 function getBoardValue(player, num) {
+    $("#buy-opt").show();
     if (boardValues[num].type === 'Property') {
-        console.log(globalPlayers[player]);
         $('#prop-info').html(`
                 <label>TITLE DEEDS</label>
                 <p>${boardValues[num].name}</p>
@@ -184,27 +210,36 @@ function getBoardValue(player, num) {
             `);
         if (boardValues[num].isOwned === false) {
             $('#buy-opt').html(`
-                    <label>${boardValues[num].name} is not owned, would you like to purchase property?</label>
+                    <label>${boardValues[num].name} is not owned, would you like to purchase property for ${boardValues[num].price}?</label>
                     <button class='btn btn-default' id='buy-prop'>I would love to!</button>
                     <button class='btn btn-default'>Nah, I'm good</button>
                 `);
             $('#buy-prop').on('click', function() {
                 globalPlayers[player].money -= boardValues[num].price;
+                boardValues[num].isOwned = true;
+                boardValues[num].owner = globalPlayers[player].uuid;
+                promptPlayer();
             });
-
         } else if (boardValues[num].isOwned === true) {
             $('#buy-opt').html(`
                     <label>${boardValues[num].name} is owned by ${boardValues[num].owner}.</label>
                     <p>You owe them money for rent.</p>
+                    <button class='btn btn-default' id='rent-owed'>Pay Owner</button>
                     `);
+            $('#rent-owed').on('click', function() {
+                globalPlayers[player].money -= boardValues[num].rent;
+                globalPlayers[boardValues[num].owner] += boardValues[num].rent;
+                promptPlayer();
+            });
         } else if (boardValues[num].isMortgaged === true) {
             $('#buy-opt').html(`
-                    <label>
+                    <label>${boardValues[num].name} is owned and mortgaged.</label>
+                    <p>Enjoy your free stay.</p>
                     `);
         }
     } else if (boardValues[num].type === 'RR') {
         $('#prop-info').html(`
-                <p>${boardValues[num].name}</p>
+                <label>${boardValues[num].name}</label>
                 <p>Rent                  ${boardValues[num].rent}</p>
                 <p>If 2 R.R.'s are owned ${boardValues[num].rentOne}</p>
                 <p>If 3 R.R.'s are owned ${boardValues[num].rentTwo}</p>
@@ -214,19 +249,29 @@ function getBoardValue(player, num) {
                 `);
         if (boardValues[num].isOwned === false) {
             $('#buy-opt').html(`
-                    <label>${boardValues[num].name} is not owned, would you like to purchase property?</label>
-                    <button class='btn btn-default'>I would love to!</button>
+                    <label>${boardValues[num].name} is not owned, would you like to purchase property for ${boardValues[num].price}?</label>
+                    <button class='btn btn-default' id='buy-prop'>I would love to!</button>
                     <button class='btn btn-default'>Nah, I'm good</button>
                 `);
+             $('#buy-prop').on('click', function() {
+                globalPlayers[player].money -= boardValues[num].price;
+                boardValues[num].isOwned = true;
+                boardValues[num].owner = globalPlayers[player].uuid;
+                promptPlayer();
+            });
         } else if (boardValues[num].isOwned === true) {
             $('#buy-opt').html(`
                     <label>${boardValues[num].name} is owned by ${boardValues[num].owner}.</label>
                     <p>You owe them money for rent.</p>
+                    <button class="btn btn-default" id="ok">OK</button>
                     `);
+            $("#ok").on('click', function() {
+                promptPlayer();
+            });
         }
-    } else if (boardValues[num].type === 'Tax') {
+    } else if (boardValues[num].type === 'Utility') {
         $('#prop-info').html(`
-                <p>${boardValues[num].name}</p>
+                <label>${boardValues[num].name}</label>
                 <p>  If one "Utility" is owned</p>
                 <p>rent is 4 times amount shown</p>
                 <p>on dice.</p>
@@ -238,19 +283,24 @@ function getBoardValue(player, num) {
                 `);
         if (boardValues[num].isOwned === false) {
             $('#buy-opt').html(`
-                    <label>${boardValues[num].name} is not owned, would you like to purchase property?</label>
-                    <button class='btn btn-default'>I would love to!</button>
+                    <label>${boardValues[num].name} is not owned, would you like to purchase property for ${boardValues[num].price}?</label>
+                    <button class='btn btn-default' id='buy-prop'>I would love to!</button>
                     <button class='btn btn-default'>Nah, I'm good</button>
                 `);
+            promptPlayer();
         } else if (boardValues[num].isOwned === true) {
             $('#buy-opt').html(`
                     <label>${boardValues[num].name} is owned by ${boardValues[num].owner}.</label>
                     <p>You owe them money for rent.</p>
+                    <button class="btn btn-default" id="ok">OK</button>
                     `);
+            $("#ok").on('click', function() {
+                promptPlayer();
+            });
         }
     } else if (boardValues[num].id === 4) {
         $('#prop-info').html(`
-                <p>${boardValues[num].name}</p>
+                <label>${boardValues[num].name}</label>
                 <br>
                 <p>Pay 10%</p>
                 <p>   or   </p>
@@ -260,26 +310,86 @@ function getBoardValue(player, num) {
                 <button class='btn btn-default'>Pay 10%</button>
                 <button class='btn btn-default'>Pay $200</button>
                 `);
+        promptPlayer();
     } else if (boardValues[num].id === 38) {
         $('#prop-info').html(`
-                <p>LUXURY TAX</p>
+                <label>LUXURY TAX</label>
                 <br>
                 <p>PAY $75.00</p>    
                 `);
+        $('#buy-opt').html(`
+                <button class='btn btn-default' id="ok">Pay $75</button>
+                `);
+        $("#ok").on('click', function() {
+            promptPlayer();
+        });
     } else if (boardValues[num].type === 'Chance') {
         $('#prop-info').html(`
-                <p>CHANCE</p>    
+                <label>CHANCE</label>    
                 `);
+        $('#buy-opt').html(`
+            <button class="btn btn-default" id="ok">OK</button>
+                `);
+        $("#ok").on('click', function() {
+            promptPlayer();
+        });
+
     } else if (boardValues[num].type === 'Chest') {
         $('#prop-info').html(`
-                <p>COMMUNITY CHEST</p>
+                <label>COMMUNITY CHEST</label>
                 `);
+        $('#buy-opt').html(`
+            <button class="btn btn-default" id="ok">OK</button>
+                `);
+        $("#ok").on('click', function() {
+            promptPlayer();
+        });
     } else if (boardValues[num].type === 'Go') {
         $('#prop-info').html(`
+                <label>GO</label>
                 <p>COLLECT</p>
                 <p>$200.00 SALARY</p>
                 <p>AS YOU PASS</p>
                 `);
+        $('#buy-opt').html(`
+            <button class="btn btn-default" id="ok">OK</button>
+                `);
+        $("#ok").on('click', function() {
+            promptPlayer();
+        });
+    } else if (boardValues[num].type === 'Free') {
+        $('prop-info').html(`
+                <label>FREE PARKING</label>
+                `);
+        $('#buy-opt').html(`
+            <button class="btn btn-default" id="ok">OK</button>
+                `);
+        $("#ok").on('click', function() {
+            promptPlayer();
+        });
+    } else if (boardValues[num].type === 'Jail') {
+        $('prop-info').html(`
+                <label>GO TO JAIL</label>
+                <p>Go directly to jail</p>
+                <p>Do not pass go,</p>
+                <p>Do not collect $200</p>
+                `);
+        $('#buy-opt').html(`
+                        <button class="btn btn-default" id="ok">OK</button>
+                `);
+        $("#ok").on('click', function() {
+            promptPlayer();
+        });
+    } else if (boardValues[num].type === 'Visiting') {
+        $('prop-info').html(`
+                <label>JUST VISITING</label>
+                `);
+        $('#buy-opt').html(`
+                        <button class="btn btn-default" id="ok">OK</button>
+                `);
+        $("#ok").on('click', function() {
+            promptPlayer();
+        });
     }
 
 }
@@ -299,10 +409,8 @@ function getPlayer(data) {
             console.log(playerPiece);
             console.log(element);
             // Append Snap.svg piece as string
-            element[element.piece] = "Snap.select(" + playerPiece + ")";
-            // Push to global snap pieces
-            snapPieces.push(element[element.piece]);
-            $(playerPiece).show();
+            element.player_piece = playerPiece;
+            // $(playerPiece).show();
         });
         console.log(snapPieces);
         return data;
